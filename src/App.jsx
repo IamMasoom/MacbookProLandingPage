143ff2f572f5741edcd39e96ae6a54d196870780
Johan <iam.masoom92@gmail.com>
2026-02-24 19:33:04 +0530

    background text added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
-import { Environment, OrbitControls } from '@react-three/drei'
+import { Environment, OrbitControls, ScrollControls } from '@react-three/drei'
+
 import MacContainer from './MacContainer'
 
 const App = () => {
   return (
     <>
-      <Canvas camera={{fov: 12, position:[0, -10, 220]}}>
-        <Environment files={["https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr"]}/>
-        <MacContainer />
-      </Canvas>
+      <div className='w-full h-screen bg-black'>
+        <div className='absolute flex flex-col items-center top-32 left-1/2 -translate-x-1/2 text-white'>
+
+          <h3 className='masked text-7xl tracking-tight font-bold' data-text="macbook pro">macbook pro</h3>
+          <h5>Oh so pro !</h5>
+          <p className='text-center w-3/4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum praesentium facere facilis!</p>
+        </div>
+        <Canvas camera={{ fov: 12, position: [0, -10, 220] }}>
+          <Environment files={["https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr"]} />
+          <ScrollControls pages={2}>
+            <MacContainer />
+          </ScrollControls>
+        </Canvas>
+
+      </div>
     </>
   )
 }
